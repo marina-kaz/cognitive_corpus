b5551fedcc8d60b054946aeb97f9597527effe6d
Kazyulina <marina.kazyulina@intel.com>
2022-06-30 14:01:55 +0300

    fix paths for an app with ngrams and concordance only

--- corpus_app/main_page.py
+++ corpus_app/main_page.py
@@ -6,11 +6,10 @@ import streamlit as st
 
 logging.info('Onto model unpacking')
 if not Path("model").is_dir():
-    shutil.unpack_archive("model.zip")
+    shutil.unpack_archive(Path.cwd() / "corpus_app" / "model.zip")
 logging.info('Onto corpora unpacking')
 if not Path("corpora").is_dir():
-    shutil.unpack_archive("corpora.zip")
-
+    shutil.unpack_archive(Path.cwd() / "corpus_app" / "corpora.zip")
 
 st.title("Работа с корпусом")
 
